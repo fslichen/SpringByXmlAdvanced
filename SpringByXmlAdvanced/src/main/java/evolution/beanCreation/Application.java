4a541a6000914b619966b61d7b995719aeb80117
user <user@use1r-PC>
2017-02-20 13:54:32 +0800

    commit

--- SpringByXmlAdvanced/src/main/java/evolution/beanCreation/Application.java
+++ SpringByXmlAdvanced/src/main/java/evolution/beanCreation/Application.java
@@ -4,7 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beanCreationApplicationContext.xml");
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext4BeanCreation.xml");
 		AnyService anyServiceByStaticMethod = applicationContext.getBean("anyServiceByStaticMethod", AnyService.class);
 		anyServiceByStaticMethod.anyMethod();
 		AnyService anyServiceByNonStaticMethod = applicationContext.getBean("anyServiceByNonStaticMethod", AnyService.class);
